f0226427ab970c7c57a74b1144be29fc2efb41cd
tsegaye <shewamaretsegaye@gmail.com>
2026-02-06 16:30:38 +0300

    problem_43

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,1 +1,13 @@
-list(sr)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        strDict = {}
+        for sr in strs:
+            curr = "".join(sorted(list(sr)))
+            if curr in strDict:
+                strDict[curr].append(sr)
+            else:
+                strDict[curr] = [sr]
+        res = []
+        for val in strDict:
+            res.append(strDict[val])
+        return res
