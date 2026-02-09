d74d337f7f605a804ff96a3a7a0d4f078001c657
tsegaye <shewamaretsegaye@gmail.com>
2026-02-09 15:59:13 +0300

    separate_the_digits

---
+++ separate_the_digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        res = []
+        def separate(num):
+            for val in str(num):
+                res.append(int(val))
+        for num in nums:
+            separate(num)
+        return res
