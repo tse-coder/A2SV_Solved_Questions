93ec8dc629c94fcff93a729feb5a8edc141459b3
Tsegaye <shewamaretsegaye@gmail.com>
2026-02-11 16:00:20 +0300

    Time: 333 ms (5.51%) | Memory: 38.3 MB (5.91%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        nums_counter = Counter(nums)
+        max_num = nums[0]
+        print("nums_counter:",nums_counter)
+        for val in nums_counter:
+            if nums_counter[val] > nums_counter[max_num]:
+                max_num = val
+        print("max_num:", max_num)
+        f1 = 0
+        f2 = nums_counter[max_num]
+        for i in range(0,len(nums)):
+            if nums[i] == max_num:
+                f1 += 1
+                f2 -= 1
+            print(f1,(i+1),f2,(len(nums)-i-1))
+            if f1 > (i+1)//2 and f2 > (len(nums)-i-1)//2:
+                return i
+        
+        return -1
