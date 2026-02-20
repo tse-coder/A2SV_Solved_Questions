14bbb2735e5e9fe0d7f1e6ab94324721261d36f9
Tsegaye <shewamaretsegaye@gmail.com>
2026-02-20 17:05:27 +0300

    Time: 2 ms (18.21%) | Memory: 19.5 MB (6.41%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,29 +1,13 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        common = set(order) & set(s)
-        res = []
-        miss = []
-        initial_cnt = Counter(s)
-        pro = set()
-        for char in order:
-            if char in initial_cnt:
-                res += [char] * initial_cnt[char]
-            pro.add(char)
-            
+        level = dict()
+        for i , c in enumerate(order):
+            level[c] = i
         for c in s:
-            if c not in pro:
-                miss.append(c)
-        # print(res)
-        # for char in s:
-        #     if char not in visited_cnt:
-        #         for _ in range(initial_cnt[char]):
-        #             res += char
-        #         visited_cnt[char] = initial_cnt[char]
-        #     elif visited_cnt[char] < initial_cnt[char]:
-        #         diff = initial_cnt[char] - visited_cnt[char]
-        #         for _ in range(diff):
-        #             res += char
-        #         visited_cnt[char]+= diff
-        res.extend(miss)
-        
-        return "".join(res)
+            if c not in order:
+                level[c] = float("inf")
+        s = list(s)
+        s.sort(key=lambda x : level[x])
+
+        return "".join(s)
+
