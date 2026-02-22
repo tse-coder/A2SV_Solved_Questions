5aa5dfa7f6f9a880e19a40564dfd455b3fcc91bc
tsegaye <shewamaretsegaye@gmail.com>
2026-02-23 00:08:10 +0300

    dominant character

--- codeforces/C_Dominant_Character.py
+++ codeforces/C_Dominant_Character.py
@@ -1,11 +1,27 @@
-t = int(input())
-for _ in range(t):
+import sys
+input = sys.stdin.readline
+
+def solve():
     n = int(input())
-    letters = input()
-    pos = ["aa","aab","baa","aba","aac","caa","aca"]
-    for val in pos:
-        if val in letters:
-            print(len(val))
-            break
-    else:
+    string = input()
+    res = float('inf')
+    if len(string) < 2:
         print(-1)
+        return
+    for i in range(n-1):
+        a = b = c = 0
+        for j in range(i,min(n, i+7)):
+            if string[j] == 'a':
+                a += 1
+            elif string[j] == 'b':
+                b += 1
+            else:
+                c += 1
+            if a > b and a > c and j - i + 1 >= 2:
+                res = min(res, j - i + 1)
+    print(-1 if res == float('inf') else res)
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        solve()
