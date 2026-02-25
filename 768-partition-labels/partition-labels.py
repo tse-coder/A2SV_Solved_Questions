58971754197199cac754e579e5f82ee3aac39d82
Tsegaye <shewamaretsegaye@gmail.com>
2026-02-25 08:45:29 +0300

    Time: 6 ms (27.44%) | Memory: 19.4 MB (22.10%) - LeetSync

---
+++ 768-partition-labels/partition-labels.py
@@ -0,0 +1,16 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        final = dict()
+        br = 0
+        for i in range(len(s)):
+            final[s[i]] = i
+        res = []
+        for i in range(len(s)):
+            br = max(br,final[s[i]])
+            if i == br:
+                if len(res) == 0:
+                    res.append(i+1)
+                else:
+                    res.append((i+1)-sum(res))
+        print(final)
+        return res
