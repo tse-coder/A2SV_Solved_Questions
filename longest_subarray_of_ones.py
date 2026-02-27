005e8c95d226ebc0d21dd5194c4ab24f05e01daa
tsegaye <shewamaretsegaye@gmail.com>
2026-02-27 14:48:20 +0300

    longest subarray of ones

---
+++ longest_subarray_of_ones.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        left = res = zeros = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zeros += 1
+            while zeros > 1:
+                if nums[left] == 0:
+                    zeros -= 1
+                    
+                left += 1
+            res = max(res,right-left)
+        return res
