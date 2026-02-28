145da10a974bd467e6bbf21f165432dd9e1192c4
tsegaye <shewamaretsegaye@gmail.com>
2026-02-28 14:58:10 +0300

    flip the bits

--- codeforces/B_Flip_the_Bits.py
+++ codeforces/B_Flip_the_Bits.py
@@ -6,15 +6,17 @@ def solve():
     a = list(map(int,list(input().strip())))
     b = list(map(int,list(input().strip())))
     ones = a.count(1)
-    zeros = a.count(1)
-    
+    zeros = a.count(0)
+
     invert = 0
     for i in range(n-1,-1,-1):
-        if a[i] != b[i]^invert:
+        if a[i]^invert != b[i]: 
+            #invert
             if zeros != ones:
-                print("NO",i)
+                print("NO")
                 return
             invert ^= 1
+
         if a[i] == 0:
             zeros -= 1
         else:
