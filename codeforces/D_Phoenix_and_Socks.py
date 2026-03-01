3bb8cfa9e5c2c49f7d1f2d733baf10bc478a48ae
tsegaye <shewamaretsegaye@gmail.com>
2026-03-01 20:39:04 +0300

    phoenix and socks

--- codeforces/D_Phoenix_and_Socks.py
+++ codeforces/D_Phoenix_and_Socks.py
@@ -1,44 +1,67 @@
 import sys
-from collections import Counter
+ 
+# sys.setrecursionlimit(10**6)
+from collections import *
+from bisect import bisect_left, bisect_right
+from copy import deepcopy
+from random import randint
+from math import *
+from heapq import *
+ 
+# input = input
 input = sys.stdin.readline
-
+ 
+ 
+def calculator(nums, l, r):
+    cost = 0
+    nums = sorted([v for _, v in nums.items()], reverse=True)
+    trans_change = l
+    for i in range(len(nums)):
+        while nums[i] > 1 and l != r:
+            nums[i] -= 2
+            cost += 1
+            r += 1
+            l -= 1
+            trans_change -= 2
+        if l == r:
+            break
+    return cost + trans_change
+ 
+ 
 def solve():
-    n,l,r = map(int, input().split())
+    n, l, r = list(map(int, input().split()))
     nums = list(map(int, input().split()))
-    lc = Counter(nums[:l])
-    rc = Counter(nums[l:])
-    
-    res = 0
-    # remove the matched pairs
-    for char in list(lc.keys()):
-        if char in rc:
-            m = min(lc[char],rc[char])
-            lc[char] -= m
-            rc[char] -= m
-            l -= m
-            r -= m
-    # make sure left side is heavier
-    if l < r:
-        r,l = l,r
-        lc,rc = rc,lc
-    # count the extra pairs in the left
-    for color in lc:
-        while lc[color] >= 2 and l > r:
-            lc[color] -= 2
-            l -= 2
-            res += 1
-
-    # flip the remaining
-    rem = (l-r)//2
-    res += rem
-    l -= rem * 2
-
-    
-    res += l
-    print(res)
-
-
-if __name__ == '__main__':
+    left = Counter(nums[:l])
+    right = Counter(nums[l:])
+ 
+    for sock in list(left.keys()):
+        if sock in right:
+            size = min(left[sock], right[sock])
+            left[sock] -= size
+            right[sock] -= size
+ 
+            l -= size
+            r -= size
+ 
+            if left[sock] == 0:
+                del left[sock]
+            if right[sock] == 0:
+                del right[sock]
+ 
+    if l == r:
+        print(l)
+    elif l > r:
+        print(calculator(left, l, r))
+    else:
+        print(calculator(right, r, l))
+ 
+ 
+def main():
     t = int(input())
+ 
     for _ in range(t):
         solve()
+ 
+ 
+if __name__ == "__main__":
+    main()
