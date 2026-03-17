fa3d333be4e763facf5d6594dfa253e2a4957f9a
Tsegaye <shewamaretsegaye@gmail.com>
2026-03-17 19:10:56 +0300

    Time: 1540 ms (20.35%) | Memory: 19.2 MB (98.08%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,10 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def helper(left,right):
+
+            if left == right:
+                return nums[left]
+            
+            return max(nums[left]-helper(left+1,right),nums[right]-helper(left,right-1))
+        
+        return helper(0,len(nums)-1) >= 0
