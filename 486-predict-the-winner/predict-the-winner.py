efa7f1bdf576350bf348faf2e5093874019d90e9
Tsegaye <shewamaretsegaye@gmail.com>
2026-03-18 18:48:16 +0300

    Time: 55 ms (22.27%) | Memory: 19.3 MB (76.99%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,11 +1,19 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        @lru_cache
-        def helper(left,right):
+        l, r = 0, len(nums) - 1
 
-            if left == right:
-                return nums[left]
+        def get_winner(p1, p2, tera, nums):
+            if len(nums) == 0:
+                return p1 >= p2
+
+            if tera == 1:
+                return (get_winner(p1 + nums[0], p2, 0, nums[1:]) or get_winner(p1 + nums[-1], p2, 0, nums[:-1]))
+            elif tera == 0:
+                return (get_winner(p1, p2 + nums[0], 1, nums[1:]) and get_winner(p1, p2 + nums[-1], 1, nums[:-1]))
+
+        return get_winner(0, 0, 1, nums)
+
+
+
+
             
-            return max(nums[left]-helper(left+1,right),nums[right]-helper(left,right-1))
-        
-        return helper(0,len(nums)-1) >= 0
