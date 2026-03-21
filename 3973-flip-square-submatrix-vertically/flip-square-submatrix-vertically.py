262eae79cef3cb010d6164673a616d045a184573
Tsegaye <shewamaretsegaye@gmail.com>
2026-03-21 17:41:19 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (69.79%) - LeetSync

---
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseSubmatrix(self, grid: List[List[int]], x: int, y: int, k: int) -> List[List[int]]:
+        t,b = x,x+k-1
+        while t < b:
+            for i in range(k):
+                grid[t][y+i],grid[b][y+i] = grid[b][y+i], grid[t][y+i]
+            t += 1
+            b -= 1
+        return grid
