40543479ec4cc6a095f57317450181f4dc3eeff9
Tsegaye <shewamaretsegaye@gmail.com>
2026-03-25 04:11:11 +0300

    Time: 115 ms (81.51%) | Memory: 41.3 MB (99.32%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.py
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.py
@@ -1,27 +1,26 @@
 class Solution:
     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
         n,m = len(grid),len(grid[0])
-        horizontal = [grid[i][j] for i in range(n) for j in range(m)]
-
-        size = len(horizontal)
-        for i in range(1,size):
-            horizontal[i] += horizontal[i-1]
-
-        canHorizontal = False
-        for i in range(m-1,size,m):
-            if horizontal[-1]-horizontal[i] == horizontal[i]:
-                canHorizontal = True
+        total_sum = 0
+        for i in range(n):
+            for j in range(m):
+                total_sum += grid[i][j]
+        if total_sum & 1:
+            return False
+        total_sum = total_sum//2
+        _sum = 0
+        for i in range(n):
+            if _sum == total_sum:
+                return True
+            if _sum > total_sum:
                 break
-        
-        vertical = [grid[i][j] for j in range(m) for i in range(n)]
-
-        for i in range(1,size):
-            vertical[i] += vertical[i-1]
-
-        canVertical = False
-        for i in range(n-1,size,n):
-            if vertical[-1] - vertical[i] == vertical[i]:
-                canVertical = True
+            _sum += sum(grid[i])
+        _sum = 0
+        for j in range(m):
+            if _sum == total_sum:
+                return True
+            if _sum > total_sum:
                 break
-            
-        return canVertical or canHorizontal
+            for i in range(n):
+                _sum += grid[i][j]
+        return False
