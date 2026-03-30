41240f926461abfdae3d4c806032aecb9367eefa
Tsegaye <shewamaretsegaye@gmail.com>
2026-03-30 18:40:44 +0300

    Time: 2136 ms (5.44%) | Memory: 19.3 MB (99.07%) - LeetSync

---
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.py
@@ -0,0 +1,29 @@
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        s = list(s)
+        res = []
+        min_diff = len(s)
+        def removePar(i,o,c,path):
+            nonlocal min_diff, res
+            if i == len(s):
+                if o != c or "".join(path) in res:
+                    return
+                len_diff = len(s) - len(path)
+                if len_diff < min_diff:
+                    res = ["".join(path)]
+                    min_diff = len_diff
+                elif len_diff == min_diff:
+                    res.append("".join(path))
+                return
+            if c > o:
+                return
+            if s[i] == "(":
+                removePar(i+1,o,c,path)
+                removePar(i+1,o+1,c,path+["("])
+            elif s[i] == ")":
+                removePar(i+1,o,c,path)
+                removePar(i+1,o,c+1,path+[")"])
+            else:
+                removePar(i+1,o,c,path+[s[i]])
+        removePar(0,0,0,[])
+        return res
