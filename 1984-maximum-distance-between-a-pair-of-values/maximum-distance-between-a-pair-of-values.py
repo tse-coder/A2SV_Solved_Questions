75fa695673eb8175ce0a2c6aa59def1492e63042
Tsegaye <shewamaretsegaye@gmail.com>
2026-04-19 09:09:41 +0300

    Time: 59 ms (52.92%) | Memory: 35.7 MB (97.25%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.py
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.py
@@ -1,24 +1,10 @@
 class Solution:
-    def find_index(self,nums,val):
-        l,r = 0,len(nums)-1
-        ans = -1
-        while l <= r :
-            m = (l+r)//2
-
-            if nums[m] >= val:
-                ans = m
-                l = m + 1
-            else:
-                r = m - 1
-        return ans
-            
-        
-
-
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        j = 0
         res = 0
-        for i, num in enumerate(nums1):
-            idx = self.find_index(nums2,num)
-            if idx >= i:
-                res = max(res,idx-i)
+        for i in range(len(nums1)):
+            while j < len(nums2) and nums2[j] >= nums1[i]:
+                j += 1
+            if j-1 >= i:
+                res = max(res,j - 1 - i)
         return res
