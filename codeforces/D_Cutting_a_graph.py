c1b7098341267ade6664a5c3ceb26e591c71c102
tse-coder <shewamaretsegaye@gmail.com>
2026-05-22 00:06:19 +0300

    cutting a graph

--- codeforces/D_Cutting_a_graph.py
+++ codeforces/D_Cutting_a_graph.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+from collections import defaultdict
 
 class DSU:
     def __init__(self,n):
@@ -29,21 +30,39 @@ class DSU:
         else:
             self.parent[rootx] = rooty
             self.rank[rooty]+= 1
+    def isconnected(self,x,y):
+        return self.find(x) == self.find(y)
 
 def solve():
     n,m,k = map(int,input().split())
     dsu = DSU(n)
+    disconnected = defaultdict(set)
+    edges = []
     for _ in range(m):
         u,v = map(int,input().split())
-        dsu.union(u,v)
-    
+        edges.append((u,v))
+    operations = []
     for _ in range(k):
         op , x , y = input().split()
-        if op == "ask":
-            print("YES" if dsu.find(int(x)) == dsu.find(int(y)) else "NO")
+        x,y = int(x),int(y)
+
         if op == "cut":
-            rootx = dsu.find(int(x))
-            rooty = dsu.find(int(y))
-            if rooty == rootx:
-                dsu.parent[int(x)] = int(x)
+            disconnected[x].add(y)
+            disconnected[y].add(x)
+        
+        operations.append((op,x,y))
+    
+    for u,v in edges:
+        if (not v in disconnected[u]) and (not u in disconnected[v]):
+            dsu.union(u,v)
+    res = []
+    for op,x,y in operations[::-1]:
+        if op=="cut":
+            dsu.union(x,y)
+        else:
+            res.append("YES" if dsu.isconnected(x,y) else "NO")
+    for ret in res[::-1]:
+        print(ret)
+            
+
 solve()
