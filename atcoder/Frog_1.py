22ceda99033a12e7ad1f4a13f14b36eed76a0c30
tse-coder <shewamaretsegaye@gmail.com>
2026-05-26 15:38:33 +0300

    frog1

---
+++ atcoder/Frog_1.py
@@ -0,0 +1,7 @@
+N = int(input())
+H = list(map(int,input().split()))
+dp = [0]*N
+dp[1] = abs(H[1]-H[0])
+for i in range(2,N):
+  dp[i] = min(dp[i-2] + abs(H[i] - H[i-2]), dp[i-1] + abs(H[i] - H[i-1]))
+print(dp[-1])
