509dfc505970ccb88036b348186be3a56a1dd451
Tsegaye <shewamaretsegaye@gmail.com>
2026-06-21 10:09:49 +0300

    Time: 156 ms (5.27%) | Memory: 32 MB (66.20%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -7,14 +7,14 @@ class Solution:
         
         for i in range(1,_max+1):
             counts[i] += counts[i-1]
-        s = [0]*len(costs)
 
         for cost in costs[::-1]:
             counts[cost] -= 1
-            s[counts[cost]] = cost
+            costs[counts[cost]] = cost
         res = 0
         curr_coins = 0
-        for coin in s:
+
+        for coin in costs:
             if curr_coins + coin <= coins:
                 curr_coins += coin
                 res += 1
